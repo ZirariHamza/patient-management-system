255647aabe1a9b4d465013a552f5f8b0422ad676
Hamza Zirari <zirari.hamza@gmail.com>
2025-09-30 21:58:30 +0200

    add request and exception handling

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -1,11 +1,11 @@
 package com.pm.patientservice.controller;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.service.PatientService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,8 +21,16 @@ public class PatientController {
 
     //to handle get requests
     @GetMapping
-    public ResponseEntity<List<PatientResponseDTO>> getPatients(){
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
         List<PatientResponseDTO> patients = patientService.getPatients();
         return ResponseEntity.ok(patients); //.ok() = status 200
+    }
+
+    //to handle post requests
+    @PostMapping
+    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+        PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
+
+        return ResponseEntity.ok(patientResponseDTO);
     }
 }
--- patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -1,7 +1,10 @@
 package com.pm.patientservice.mapper;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
 
@@ -12,6 +15,18 @@ public class PatientMapper {
         patientDTO.setEmail(patient.getEmail());
         patientDTO.setAddress(patient.getAddress());
         patientDTO.setDateOfBirth(patient.getdateOfBirth().toString());
+
         return patientDTO;
+    }
+
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setdateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+
+        return patient;
     }
 }
